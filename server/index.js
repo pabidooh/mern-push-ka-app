2d29802848a597f34abb951020b2621838894830
Prodanik <prodanik.a.a@yandex.ru>
2022-04-24 21:08:34 +0500

    main

--- server/index.js
+++ server/index.js
@@ -1,32 +1,34 @@
-const express = require("express");
+const express = require("express"); // require обеспечивает асинхронную загрузку модуля
 const cors = require("cors");
 const mongoose = require("mongoose");
+
 const authRoutes = require("./routes/auth");
 const messageRoutes = require("./routes/messages");
-const app = express();
+
+const app = express(); //вызов экспресс функции
 const socket = require("socket.io");
-require("dotenv").config();
+require("dotenv").config(); // для соединения с БД
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); //для запросов POST и PUT, чтобы отправлять данные
 
-mongoose
+mongoose // передаём .env 
   .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    useNewUrlParser: true, //передаём новый синтаксический анализатоор
+    useUnifiedTopology: true, //говорим, что хотим использовать унифицированную топологию
   })
   .then(() => {
-    console.log("DB Connetion Successfull");
+    console.log("БД коннект фурычит"); //промис, выполняется, когда мы подкдючились к БД
   })
-  .catch((err) => {
+  .catch((err) => { //если есть ошибки, то ловим их
     console.log(err.message);
   });
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-const server = app.listen(process.env.PORT, () =>
-  console.log(`Server started on ${process.env.PORT}`)
+const server = app.listen(process.env.PORT, () => //мы передадим функцию обратного вызова
+  console.log(`Сервак стартовал на порте ${process.env.PORT}`) 
 );
 const io = socket(server, {
   cors: {
